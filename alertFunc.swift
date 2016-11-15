03d29902854d1bbd4286b91777812d3d1ab8b870
Andrij Trubchanin <trand@trandmac.local>
2016-11-15 14:22:43 +0200

    alertfunc

---
+++ alertFunc.swift
@@ -0,0 +1,15 @@
+    func alertFunc(){
+        let alertVC = UIAlertController(
+            title: "Alert Title",
+            message: "Alert Message",
+            preferredStyle: .alert)
+        let okAction = UIAlertAction(
+            title: "OK",
+            style:.default,
+            handler: nil)
+        alertVC.addAction(okAction)
+        present(
+            alertVC,
+            animated: true,
+            completion: nil)
+    }
